02b2192691710aace2f3fe8a5caaf05278748cb4
minung.han <hmu332233@gmail.com>
2019-03-26 00:59:45 +0900

    [Init] chrome extension

---
+++ extensionSrc/contentScript/index.js
@@ -0,0 +1,13 @@
+// contentScript.js
+
+// inject dov
+var app = document.createElement('div');
+app.id = "goormide-tools";
+document.body.prepend(app);
+
+// inject script
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('build/src.ffeef517.js');
+(document.head || document.documentElement).appendChild(s);
+
+s.parentNode.removeChild(s);
