eebebc062296a340c9535678ad98fc9e8fdc0761
minung.han <hmu332233@gmail.com>
2019-03-28 02:26:10 +0900

    [Modify] 단축키 방식 수정

--- src/apps/MainApp/MainApp.jsx
+++ src/apps/MainApp/MainApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './MainApp.scss';
 
@@ -7,6 +7,19 @@ import ToolBox from 'containers/ToolBox';
 
 function MainApp(props) {
   const [isOpen, toggle] = useToggle(false);
+  useEffect(() => {
+    // FIXME: 이벤트를 계속 add, remove를 반복하는데 이렇게 하지 않고 할 방법 찾기
+    const handleShortcut = e => {
+      if (e.ctrlKey && e.shiftKey && e.which == 76) {
+        toggle();
+      }
+    };
+    document.addEventListener('keyup', handleShortcut, false);
+    return () => {
+      document.removeEventListener('keyup', handleShortcut, false);
+    };
+  }, [isOpen]);
+
   return (
     <div className={styles.MainApp}>
       {isOpen && <ToolBox />}
