f5d630021528d9bcd0c6f71c0534c133d6f27dd1
minung.han <hmu332233@gmail.com>
2019-07-06 03:45:07 +0900

    build: change bundler from parcel to webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  // mode: 'production',
+  entry: {
+    contentScripts: './src/contentScripts/index.js',
+    app: './src/app/index.js',
+  },
+  output: {
+    path: `${ __dirname}/build`,
+    filename: '[name].js',
+    publicPath: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules',
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          },
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+  optimization: {
+    // minimize: true
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.jsx', '.js', '.scss']
+  }
+};
